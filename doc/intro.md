1b14c12272f6c50bd7c9ff93fe2bed48f6c12443
marmaluke <mark.s.lemmon@gmail.com>
2013-08-12 19:12:47 +1200

    Initialise project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to legend-bestiary
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/legend_bestiary/core_test.clj
@@ -0,0 +1,7 @@
+(ns legend-bestiary.core-test
+  (:use clojure.test
+        legend-bestiary.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
